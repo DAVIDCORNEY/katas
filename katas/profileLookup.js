e12aebb3e21ae70d8c615dd2ab50701b34b53687
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-26 15:57:59 +0100

    profileLookup completed TDD

--- katas/profileLookup.js
+++ katas/profileLookup.js
@@ -1,3 +1,10 @@
-const profileLookup = (name, prop) => {};
+const profileLookup = (arr, name, prop) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].firstName === name) {
+      return arr[i][prop] ? arr[i][prop] : "No such property";
+    }
+  }
+  return "No such contact";
+};
 
 module.exports = { profileLookup };
--- spec/profileLookup.spec.js
+++ spec/profileLookup.spec.js
@@ -1,2 +1,73 @@
 const { expect } = require("chai");
 const { profileLookup } = require("../katas/profileLookup");
+
+describe.only("profileLookup()", () => {
+  it("returns 'No such contact' if passed a name that does not correspond to a contacts firstName", () => {
+    const contacts = [
+      {
+        firstName: "Bill",
+        lastName: "Bailey",
+        number: "unknown",
+        likes: ["JavaScript", "Pizza", "Wine"]
+      }
+    ];
+    expect(profileLookup(contacts, "David")).to.equal("No such contact");
+  });
+  it("returns the value of the passed prop argument if name corresponds to a contacts firstName and prop is a property of the contact ", () => {
+    const contacts = [
+      {
+        firstName: "David",
+        lastName: "Smith",
+        number: "unknown",
+        likes: ["JavaScript", "React", "Pasta"]
+      }
+    ];
+    expect(profileLookup(contacts, "David", "lastName")).to.equal("Smith");
+    expect(profileLookup(contacts, "David", "likes")).to.eql([
+      "JavaScript",
+      "React",
+      "Pasta"
+    ]);
+  });
+  it("returns 'No such property' if a prop does not correspond to any valid properties of a contact found to match name", () => {
+    const contacts = [
+      {
+        firstName: "David",
+        lastName: "Smith",
+        number: "unknown",
+        likes: ["JavaScript", "React", "Pasta"]
+      }
+    ];
+    expect(profileLookup(contacts, "David", "age")).to.equal(
+      "No such property"
+    );
+  });
+  it("returns the value of the passed prop argument if name corresponds to a contacts firstName and prop is a property of the contact, when passed an array of multiple contacts ", () => {
+    const contacts = [
+      {
+        firstName: "David",
+        lastName: "Smith",
+        number: "unknown",
+        likes: ["JavaScript", "React", "Pasta"]
+      },
+      {
+        firstName: "Bill",
+        lastName: "Bailey",
+        number: "0994372684",
+        likes: ["Javascript", "Pizza", "Wine"]
+      },
+      {
+        firstName: "Julie",
+        lastName: "Bentley",
+        number: "0543236543",
+        likes: ["Coding", "Tacos", "Skating"]
+      }
+    ];
+    expect(profileLookup(contacts, "Bill", "lastName")).to.equal("Bailey");
+    expect(profileLookup(contacts, "Bill", "likes")).to.eql([
+      "Javascript",
+      "Pizza",
+      "Wine"
+    ]);
+  });
+});
