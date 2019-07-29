570aed959d28b2fdabb6a24d404c4078caf6588a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-29 15:57:09 +0100

    sumAllNumbers completed TDD

--- katas/sumAllNumbers.js
+++ katas/sumAllNumbers.js
@@ -1,3 +1,10 @@
-function sumAllNumbers() {}
+function sumAllNumbers(arr) {
+  let total = 0;
+  if (arr[0] === arr[1]) return arr[0] + arr[1];
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    total += i;
+  }
+  return total;
+}
 
 module.exports = { sumAllNumbers };
--- spec/sumAllNumbers.spec.js
+++ spec/sumAllNumbers.spec.js
@@ -1,5 +1,29 @@
+const { expect } = require("chai");
 const { sumAllNumbers } = require("../katas/sumAllNumbers");
 
 describe("sumAllNumbers()", () => {
-  it("", () => {});
+  it("returns the sum of two numbers when passed an array of two positive numbers of the same value", () => {
+    const arr = [1, 1];
+    expect(sumAllNumbers(arr)).to.equal(2);
+  });
+  it("returns the sum of two numbers when passed an array of two consecutive positive numbers", () => {
+    const arr = [1, 2];
+    expect(sumAllNumbers(arr)).to.equal(3);
+  });
+  it("returns the sum of two numbers and the numbers in between, when passed an array of two positive numbers in ascending order", () => {
+    const arr = [1, 3];
+    expect(sumAllNumbers(arr)).to.equal(6);
+    const arr2 = [1, 4];
+    expect(sumAllNumbers(arr2)).to.equal(10);
+    const arr3 = [5, 10];
+    expect(sumAllNumbers(arr3)).to.equal(45);
+  });
+  it("returns the sum of two numbers and the numbers in between, when passed an array of two positive numbers in descending order", () => {
+    const arr = [3, 1];
+    expect(sumAllNumbers(arr)).to.equal(6);
+    const arr2 = [4, 1];
+    expect(sumAllNumbers(arr2)).to.equal(10);
+    const arr3 = [10, 5];
+    expect(sumAllNumbers(arr3)).to.equal(45);
+  });
 });
