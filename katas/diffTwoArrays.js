acbfe58710a9f65f899ae6dddc0737fdc6b4903f
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-30 12:50:08 +0100

    diffTwoArrays completed TDD

--- katas/diffTwoArrays.js
+++ katas/diffTwoArrays.js
@@ -1,3 +1,7 @@
-function diffTwoArrays(arr1, arr2) {}
+function diffTwoArrays(array1, array2) {
+  return array1.concat(array2).filter(item => {
+    return !array1.includes(item) || !array2.includes(item);
+  });
+}
 
 module.exports = { diffTwoArrays };
--- spec/diffTwoArrays.spec.js
+++ spec/diffTwoArrays.spec.js
@@ -1,6 +1,42 @@
 const { expect } = require("chai");
-const diffTwoArrays = require("../katas/diffTwoArrays");
+const { diffTwoArrays } = require("../katas/diffTwoArrays");
 
-describe("diffTwoArrays()", () => {
-  it("", () => {});
+describe.only("diffTwoArrays()", () => {
+  it("should return an empty array if passed two empty arrays", () => {
+    const arr1 = [];
+    const arr2 = [];
+    expect(diffTwoArrays(arr1, arr2)).to.eql([]);
+  });
+  it("should return an array of one item, if passed an array with one item and an empty array", () => {
+    const arr1 = [1];
+    const arr2 = [];
+    expect(diffTwoArrays(arr1, arr2)).to.eql([1]);
+  });
+  it("should return an empty array, if passed two arrays, each with a single indentical item", () => {
+    const arr1 = [1];
+    const arr2 = [1];
+    expect(diffTwoArrays(arr1, arr2)).to.eql([]);
+  });
+  it("should return an array of two items, if passed two arrays, each with a different item", () => {
+    const arr1 = [1];
+    const arr2 = [2];
+    expect(diffTwoArrays(arr1, arr2)).to.eql([1, 2]);
+  });
+  it("should return an array with items only found in one of the two given arrays", () => {
+    const arr1 = ["a"];
+    const arr2 = ["a", "b"];
+    expect(diffTwoArrays(arr1, arr2)).to.eql(["b"]);
+    const arr3 = ["apple", "banana"];
+    const arr4 = ["apple", "grapes", "oranges", "banana"];
+    expect(diffTwoArrays(arr3, arr4)).to.eql(["grapes", "oranges"]);
+  });
+  it("should return a new array without mutating the original input array", () => {
+    const arr1 = ["apple", "banana"];
+    const copyArr1 = [...arr1];
+    const arr2 = ["apple", "grapes", "oranges", "banana"];
+    const copyArr2 = [...arr2];
+    expect(diffTwoArrays(arr1, arr2)).to.eql(["grapes", "oranges"]);
+    expect(arr1).to.eql(copyArr1);
+    expect(arr2).to.eql(copyArr2);
+  });
 });
