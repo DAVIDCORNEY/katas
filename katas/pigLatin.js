55d71a5e3288d74845f6b600fda2afd3c29c29e8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 14:03:15 +0100

    Refactor return to use ternary

--- katas/pigLatin.js
+++ katas/pigLatin.js
@@ -4,12 +4,7 @@ function pigLatin(str) {
   const consonants = str.slice(0, vowelPosition);
   const vowels = str.slice(vowelPosition);
   if (str.length === 0) return str;
-  if (regex.test(str[0])) {
-    return `${str}way`;
-  }
-  if (regex.test(str[0] === false)) {
-    return `${vowels}${consonants}ay`;
-  }
+  return regex.test(str[0]) ? `${str}way` : `${vowels}${consonants}ay`;
 }
 
 module.exports = { pigLatin };
