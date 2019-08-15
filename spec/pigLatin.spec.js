a83f259c340fa6559d9493b5d324ac97b78d46bc
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 14:06:29 +0100

    Edge case tests test2

--- spec/pigLatin.spec.js
+++ spec/pigLatin.spec.js
@@ -7,6 +7,8 @@ describe.only("pigLatin()", () => {
   });
   it('should return a string with "way" added to the end if passed a string beginning with a vowel', () => {
     expect(pigLatin("a")).to.equal("away");
+    expect(pigLatin("algorithm")).to.equal("algorithmway");
+    expect(pigLatin("eight")).to.equal("eightway");
   });
   it('should return a string with a consonant moved to the end and suffixed with "ay" if passed a string beginning with a consonant', () => {
     expect(pigLatin("be")).to.equal("ebay");
