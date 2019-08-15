62c4fe3a5a5f3b258ec7738b06d3664479e676de
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 12:47:44 +0100

    write 2nd test

--- spec/pigLatin.spec.js
+++ spec/pigLatin.spec.js
@@ -5,4 +5,7 @@ describe.only("pigLatin()", () => {
   it("should return an empty string if passed an empty string", () => {
     expect(pigLatin("")).to.equal("");
   });
+  it('should return a string with "way" added to the end if passed a string beginning with a vowel', () => {
+    expect(pigLatin("a")).to.equal("away");
+  });
 });
