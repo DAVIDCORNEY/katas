5d545a9360b1c254aced8baf6afd0885c946ffb8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 13:04:18 +0100

    write 3rd test

--- spec/pigLatin.spec.js
+++ spec/pigLatin.spec.js
@@ -8,4 +8,7 @@ describe.only("pigLatin()", () => {
   it('should return a string with "way" added to the end if passed a string beginning with a vowel', () => {
     expect(pigLatin("a")).to.equal("away");
   });
+  it('should return a string with a consonant moved to the end and suffixed with "ay" if passed a string beginning with a consonant', () => {
+    expect(pigLatin("be")).to.equal("ebay");
+  });
 });
