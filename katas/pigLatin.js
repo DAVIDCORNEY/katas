fd35bcfa8d4d7af00639cf253fefb94351171ec7
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 13:57:20 +0100

    passing test 3

--- katas/pigLatin.js
+++ katas/pigLatin.js
@@ -1,7 +1,14 @@
 function pigLatin(str) {
+  let regex = /[aeiou]/;
   if (str.length === 0) return str;
-  if (/[aeiou]/gi.test(str[0])) {
+  if (regex.test(str[0])) {
     return `${str}way`;
+  }
+  if (regex.test(str[0] === false)) {
+    const vowelPosition = str.search(regex);
+    const consonants = str.slice(0, vowelPosition);
+    const vowels = str.slice(vowelPosition);
+    return `${vowels}${consonants}ay`;
   }
 }
 
