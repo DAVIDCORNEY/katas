edce17f6a88ad5ed4001fddc89c5ef592b0efbb4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-15 13:59:25 +0100

    Move variables out of if statements

--- katas/pigLatin.js
+++ katas/pigLatin.js
@@ -1,13 +1,13 @@
 function pigLatin(str) {
-  let regex = /[aeiou]/;
+  const regex = /[aeiou]/;
+  const vowelPosition = str.search(regex);
+  const consonants = str.slice(0, vowelPosition);
+  const vowels = str.slice(vowelPosition);
   if (str.length === 0) return str;
   if (regex.test(str[0])) {
     return `${str}way`;
   }
   if (regex.test(str[0] === false)) {
-    const vowelPosition = str.search(regex);
-    const consonants = str.slice(0, vowelPosition);
-    const vowels = str.slice(vowelPosition);
     return `${vowels}${consonants}ay`;
   }
 }
