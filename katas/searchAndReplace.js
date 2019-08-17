6eb43a2973d268baa5f68438c185c6fa6af0ad70
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-17 11:27:06 +0100

    Refactor  if statement

--- katas/searchAndReplace.js
+++ katas/searchAndReplace.js
@@ -1,5 +1,10 @@
 function searchAndReplace(str, before, after) {
   if (str.length === 0) return "";
+  if (before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
   let newStr = str.replace(before, after);
   return newStr;
 }
