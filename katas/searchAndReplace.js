740be2f448cdf62741721a95813841365d450661
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-17 09:33:12 +0100

    Passing test 2

--- katas/searchAndReplace.js
+++ katas/searchAndReplace.js
@@ -1,5 +1,7 @@
 function searchAndReplace(str, before, after) {
   if (str.length === 0) return "";
+  let newStr = str.replace(before, after);
+  return newStr;
 }
 
 module.exports = { searchAndReplace };
