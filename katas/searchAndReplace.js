f8aad10e0cb798d3122bdfd420d82a5cfafe3be0
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-17 12:04:54 +0100

    Refactor searchAndReplace js

--- katas/searchAndReplace.js
+++ katas/searchAndReplace.js
@@ -1,10 +1,8 @@
 function searchAndReplace(str, before, after) {
   if (str.length === 0) return "";
-  if (before[0] === before[0].toUpperCase()) {
-    after = after[0].toUpperCase() + after.slice(1);
-  } else {
-    after = after[0].toLowerCase() + after.slice(1);
-  }
+  before[0] === before[0].toUpperCase()
+    ? (after = after[0].toUpperCase() + after.slice(1))
+    : (after = after[0].toLowerCase() + after.slice(1));
   let newStr = str.replace(before, after);
   return newStr;
 }
