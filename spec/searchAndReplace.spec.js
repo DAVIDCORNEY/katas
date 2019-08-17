5fa6a7b45fe1b39040a17ac990279d40c14527fc
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-17 09:25:22 +0100

    Passing first test

--- spec/searchAndReplace.spec.js
+++ spec/searchAndReplace.spec.js
@@ -1,6 +1,8 @@
-const { chai } = require("chai");
+const { expect } = require("chai");
 const { searchAndReplace } = require("../katas/searchAndReplace");
 
 describe.only("searchAndReplace()", () => {
-  it("", () => {});
+  it("should return an empty string if passed an empty string", () => {
+    expect(searchAndReplace("")).to.equal("");
+  });
 });
