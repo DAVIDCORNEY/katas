bf01bb983c21f2c36b90c26f6bcbc9453d890989
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-17 12:15:36 +0100

    SearchAndReplace complete remove .only

--- spec/searchAndReplace.spec.js
+++ spec/searchAndReplace.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { searchAndReplace } = require("../katas/searchAndReplace");
 
-describe.only("searchAndReplace()", () => {
+describe("searchAndReplace()", () => {
   it("should return an empty string if passed an empty string", () => {
     expect(searchAndReplace("")).to.equal("");
   });
