95860d9d75c7570117c7f4b832229acc33d0b1a4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-19 13:31:14 +0100

    dnaPairing passing test 2

--- katas/dnaPairing.js
+++ katas/dnaPairing.js
@@ -1,5 +1,18 @@
 function dnaPairing(str) {
-  if (str === "") return "";
+  let outerArray = [];
+  let pairArray = [];
+  if (str === "") return outerArray;
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case "G":
+        pairArray.push("G", "C");
+        break;
+      default:
+        console.log("Sorry does not match");
+    }
+    outerArray.push(pairArray);
+  }
+  return outerArray;
 }
 
 module.exports = { dnaPairing };
