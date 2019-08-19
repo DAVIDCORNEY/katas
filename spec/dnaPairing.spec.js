fb530c34671f3c5d33daa4e6a152cbdbb698f106
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-19 13:05:15 +0100

    dnaPairing spec file add .only

--- spec/dnaPairing.spec.js
+++ spec/dnaPairing.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { dnaPairing } = require("../katas/dnaPairing");
 
-describe("dnaPairing()", () => {
+describe.only("dnaPairing()", () => {
   it("should return an empty string when passed an empty string", () => {
     expect(dnaPairing("")).to.equal("");
   });
