a34fec6ff2d83359f481e09a6c11f12c80c30671
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-19 14:43:16 +0100

    dnaPairing code changed to use map on a look up object

--- katas/dnaPairing.js
+++ katas/dnaPairing.js
@@ -1,25 +1,38 @@
 function dnaPairing(str) {
   let outerArray = [];
   if (str === "") return outerArray;
-  for (let i = 0; i < str.length; i++) {
-    switch (str[i]) {
-      case "G":
-        outerArray.push(["G", "C"]);
-        break;
-      case "C":
-        outerArray.push(["C", "G"]);
-        break;
-      case "A":
-        outerArray.push(["A", "T"]);
-        break;
-      case "T":
-        outerArray.push(["T", "A"]);
-        break;
-      default:
-        console.log("Sorry does not match");
-    }
-  }
-  return outerArray;
+  const dna = {
+    G: "C",
+    C: "G",
+    A: "T",
+    T: "A"
+  };
+  return str.split("").map(letter => [letter, dna[letter]]);
 }
+
+// Revised to use a lookup object and map
+// function dnaPairing(str) {
+//   let outerArray = [];
+//   if (str === "") return outerArray;
+//   for (let i = 0; i < str.length; i++) {
+//     switch (str[i]) {
+//       case "G":
+//         outerArray.push(["G", "C"]);
+//         break;
+//       case "C":
+//         outerArray.push(["C", "G"]);
+//         break;
+//       case "A":
+//         outerArray.push(["A", "T"]);
+//         break;
+//       case "T":
+//         outerArray.push(["T", "A"]);
+//         break;
+//       default:
+//         console.log("Sorry does not match");
+//     }
+//   }
+//   return outerArray;
+// }
 
 module.exports = { dnaPairing };
