f635f967e634ed6a606417d1db40b47220adb7d4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-19 13:12:13 +0100

    dnaPairing write test 2

--- spec/dnaPairing.spec.js
+++ spec/dnaPairing.spec.js
@@ -2,7 +2,10 @@ const { expect } = require("chai");
 const { dnaPairing } = require("../katas/dnaPairing");
 
 describe.only("dnaPairing()", () => {
-  it("should return an empty string when passed an empty string", () => {
-    expect(dnaPairing("")).to.equal("");
+  it("should return an empty array when passed an empty string", () => {
+    expect(dnaPairing("")).to.eql([]);
+  });
+  it("should return a 2D array with an element and its pair, when passed a string of a single element ", () => {
+    expect(dnaPairing("G")).to.eql([["G", "C"]]);
   });
 });
