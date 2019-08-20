0a8e1b066d80efc09f3cfcf5a5b5175bae0777b2
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-20 18:23:18 +0100

    missingLetters write test 2

--- spec/missingLetters.spec.js
+++ spec/missingLetters.spec.js
@@ -5,4 +5,7 @@ describe.only("missingLetters()", () => {
   it("should return an empty string when passed an empty string", () => {
     expect(missingLetters("")).to.equal("");
   });
+  it("should return the missing letter when passed a string of three consecutive letters", () => {
+    expect(missingLetters("ac")).to.equal("b");
+  });
 });
