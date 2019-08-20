67080109a5d0576fb6388f11948e2e6c7ba5de02
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-20 18:16:43 +0100

    missingLetters write test 1

--- spec/missingLetters.spec.js
+++ spec/missingLetters.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
 const { missingLetters } = require("../katas/missingLetters");
 
-describe(missingLetters(), () => {
-  it("", () => {});
+describe("missingLetters()", () => {
+  it("should return an empty string when passed an empty string", () => {
+    expect(missingLetters("")).to.equal("");
+  });
 });
