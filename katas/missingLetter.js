3463a8a3f38e268e464d43e27f079826e251b0d9
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-20 18:52:38 +0100

    missingLetter.js passing test 2

--- katas/missingLetter.js
+++ katas/missingLetter.js
@@ -3,8 +3,6 @@ function missingLetter(str) {
   for (let i = 0; i < str.length; i++) {
     if (str[i + 1].charCodeAt() - str[i].charCodeAt() === 2) {
       return String.fromCharCode(str[i].charCodeAt() + 1);
-    } else {
-      console.log(false);
     }
   }
 }
