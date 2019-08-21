81e9df6b693227623d0dd3607c0b4983c1cd4f52
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-21 13:32:49 +0100

    sortedUnion write test 2

--- spec/sortedUnion.spec.js
+++ spec/sortedUnion.spec.js
@@ -5,4 +5,7 @@ describe.only("sortedUnion()", () => {
   it("should return an empty array if passed two empty arrays", () => {
     expect(sortedUnion([], [])).to.eql([]);
   });
+  it("should return an array of two numbers if passed two arrays each with a unique number", () => {
+    expect(sortedUnion([1], [2])).to.eql([1, 2]);
+  });
 });
