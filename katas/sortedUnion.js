460224bc981e17cd877123508299c9b32707c785
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-21 14:46:09 +0100

    sortedUnion passing test 3

--- katas/sortedUnion.js
+++ katas/sortedUnion.js
@@ -1,6 +1,9 @@
 function sortedUnion(...args) {
-  const newArr = [];
-  return newArr.concat(...args);
+  const arr = [];
+  const newArr = arr.concat(...args);
+  return newArr.filter((num, i) => {
+    return newArr.indexOf(num) === i;
+  });
 }
 
 module.exports = { sortedUnion };
