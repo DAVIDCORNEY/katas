3058dc239dd12a42493e27dcdec4bb05a730b421
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-22 09:58:55 +0100

    write test 3

--- spec/convertHTML.spec.js
+++ spec/convertHTML.spec.js
@@ -5,7 +5,10 @@ describe.only("convertHTML()", () => {
   it("should return an empty string when passed an empty string", () => {
     expect(convertHTML("")).to.equal("");
   });
-  it("should return a character if passed a string of a character", () => {
+  it("should return a character if passed a string of a character without specified HTML entities", () => {
     expect(convertHTML("a")).to.equal("a");
+  });
+  it("should return words if passed a string of words without specified HTML entities", () => {
+    expect(convertHTML("Hello")).to.equal("Hell");
   });
 });
