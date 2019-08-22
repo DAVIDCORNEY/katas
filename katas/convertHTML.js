2206132c0a556de50673bb91dac76f7b86dd55b2
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-22 10:37:04 +0100

    passing test 5

--- katas/convertHTML.js
+++ katas/convertHTML.js
@@ -1,6 +1,15 @@
 function convertHTML(str) {
+  const entities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
   if (str.length === 0) return "";
-  return str.replace(/&/g, "&amp;");
+  return str.replace(/[&<]/g, char => {
+    return entities[char];
+  });
 }
 
 module.exports = { convertHTML };
