9fa78f06d9235b1e13f19d93857a2bac6955e41c
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-22 09:49:49 +0100

    convertHTML spec add .only

--- spec/convertHTML.spec.js
+++ spec/convertHTML.spec.js
@@ -1,6 +1,8 @@
-const expect = require("chai");
+const { expect } = require("chai");
 const { convertHTML } = require("../katas/convertHTML");
 
-describe("convertHTML()", () => {
-  it("", () => {});
+describe.only("convertHTML()", () => {
+  it("should return an empty string when passed an empty string", () => {
+    expect(convertHTML("")).to.equal("");
+  });
 });
