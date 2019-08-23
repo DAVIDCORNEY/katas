43ef41098f6831f88045e73dc533d1a1df1c4172
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-23 15:05:31 +0100

    fibNums test 2 passing

--- katas/sumFibs.js
+++ katas/sumFibs.js
@@ -1,5 +1,20 @@
 function sumFibs(num) {
+  const fibNums = [1, 1];
+  let index = 0;
+  let count = 0;
   if (num <= 1) return num;
+  while (count < num) {
+    fibNums.push(fibNums[index] + fibNums[index + 1]);
+    index++;
+    count = fibNums[fibNums.length - 1];
+  }
+  const sum = fibNums.reduce((acc, curr) => {
+    if (curr % 2 === 1) {
+      acc += curr;
+    }
+    return acc;
+  }, 0);
+  return sum;
 }
 
 module.exports = { sumFibs };
