c42e6a4b76eec16eafcf1809b649ac3da3d44c01
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-23 13:26:38 +0100

    sumFibs write test 1

--- spec/sumFibs.spec.js
+++ spec/sumFibs.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
 const { sumFibs } = require("../katas/sumFibs");
 
-describe("sumFibs()", () => {
-  it("", () => {});
+describe.only("sumFibs()", () => {
+  it("should return 1 if passed an integer of 1", () => {
+    expect(sumFibs(1)).to.equal(1);
+  });
 });
