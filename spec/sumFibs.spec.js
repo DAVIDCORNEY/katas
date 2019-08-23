60f4e1f0a5d3c43dd773b1e5c29f1faea4227023
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-23 15:41:17 +0100

    sumFibs passing edge cases

--- spec/sumFibs.spec.js
+++ spec/sumFibs.spec.js
@@ -8,5 +8,8 @@ describe.only("sumFibs()", () => {
   it("should return the sum of all odd Fibonacci Numbers when passed an integer of num", () => {
     expect(sumFibs(3)).to.equal(5);
     expect(sumFibs(4)).to.equal(5);
+    expect(sumFibs(1000)).to.equal(1785);
+    expect(sumFibs(75024)).to.equal(60696);
+    expect(sumFibs(75025)).to.equal(135721);
   });
 });
