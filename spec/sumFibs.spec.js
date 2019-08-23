4f341bfb10ddca6f3158dfb6a2e07f25b7332aea
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-23 13:32:14 +0100

    sumFibs write test 2

--- spec/sumFibs.spec.js
+++ spec/sumFibs.spec.js
@@ -5,4 +5,7 @@ describe.only("sumFibs()", () => {
   it("should return 1 if passed an integer of 1", () => {
     expect(sumFibs(1)).to.equal(1);
   });
+  it("should return the sum of all odd Fibonacci Numbers when passed an integer of num", () => {
+    expect(sumFibs(3)).to.equal(5);
+  });
 });
