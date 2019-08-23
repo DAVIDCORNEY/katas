5825f486753a1b4a325932bc89fd869c291fdfca
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-23 17:09:07 +0100

    sumFibs passing all tests remove .only

--- spec/sumFibs.spec.js
+++ spec/sumFibs.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { sumFibs } = require("../katas/sumFibs");
 
-describe.only("sumFibs()", () => {
+describe("sumFibs()", () => {
   it("should return 1 if passed an integer of 1", () => {
     expect(sumFibs(1)).to.equal(1);
   });
