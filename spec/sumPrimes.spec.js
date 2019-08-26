b28f81f249efdebabfedace8cee8cb71448cc0cf
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-26 15:26:36 +0100

    sumPrimes write test 2

--- spec/sumPrimes.spec.js
+++ spec/sumPrimes.spec.js
@@ -5,4 +5,7 @@ describe.only("sumPrimes()", () => {
   it("should return a number if passed a number", () => {
     expect(sumPrimes(1)).to.be.a("number");
   });
+  it("should return the sum of all prime numbers up to and including the passed number ", () => {
+    expect(sumPrimes(10)).to.equal(17);
+  });
 });
