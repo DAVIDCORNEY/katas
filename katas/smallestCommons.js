038122774584e4d137fca3b8261a8d60157f184d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-27 08:20:44 +0100

    smallestCommons add sudo code

--- katas/smallestCommons.js
+++ katas/smallestCommons.js
@@ -1,3 +1,7 @@
+//change array so in ascending order
+//loop through from min to max checking if all the nums are divisible by max
+//if not increase max by itself and do the same procedure again
+
 function smallestCommons(arr) {
   return arr[0];
 }
