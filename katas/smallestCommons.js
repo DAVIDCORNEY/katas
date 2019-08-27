66c214112e5289e60ed36d3aceae7ac5c86d768e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-27 10:04:26 +0100

    smallestCommons passing test 2

--- katas/smallestCommons.js
+++ katas/smallestCommons.js
@@ -3,7 +3,29 @@
 //if not increase max by itself and do the same procedure again
 
 function smallestCommons(arr) {
-  return arr[0];
+  let max = Math.max(...arr);
+  const min = Math.min(...arr);
+  let maxNum = max;
+
+  const newArr = [];
+  for (let i = min; i <= max; i++) {
+    newArr.push(i);
+  }
+
+  function checkDividers(newArr, maxNum) {
+    for (let i = 0; i < newArr.length; i++) {
+      if (maxNum % i !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  if (checkDividers(newArr, maxNum) === false) {
+    maxNum += max;
+    return maxNum;
+  }
+  return max;
 }
 
 module.exports = { smallestCommons };
