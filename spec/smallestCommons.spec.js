dbda6795dccfa037c884b746daf09e5d72f70da1
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-27 08:20:21 +0100

    smallestCommons write test 2

--- spec/smallestCommons.spec.js
+++ spec/smallestCommons.spec.js
@@ -5,4 +5,7 @@ describe.only("smallestCommons()", () => {
   it("should return a number when passed an array of two numbers", () => {
     expect(smallestCommons([1, 2])).to.be.a("number");
   });
+  it("should return the smallest common multiple of both numbers in the passed array and be evenly divisible by all numbers between them", () => {
+    expect(smallestCommons(1, 3)).to.equal(6);
+  });
 });
