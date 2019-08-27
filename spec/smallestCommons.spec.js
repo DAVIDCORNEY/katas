bbc9f2e26b58f3859ed4fad17e738275eb8e630b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-27 10:03:57 +0100

    smallestCommons revise test 1

--- spec/smallestCommons.spec.js
+++ spec/smallestCommons.spec.js
@@ -1,11 +1,11 @@
 const { expect } = require("chai");
 const { smallestCommons } = require("../katas/smallestCommons");
 
-describe.only("smallestCommons()", () => {
+describe("smallestCommons()", () => {
   it("should return a number when passed an array of two numbers", () => {
     expect(smallestCommons([1, 2])).to.be.a("number");
   });
-  it("should return the smallest common multiple of both numbers in the passed array and be evenly divisible by all numbers between them", () => {
-    expect(smallestCommons(1, 3)).to.equal(6);
+  it.only("should return the smallest common multiple of both numbers in the passed array and be evenly divisible by all numbers between them", () => {
+    expect(smallestCommons([3, 1])).to.equal(6);
   });
 });
