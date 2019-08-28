42bd92d9608a1609278dea636c008ecc025b3c57
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 07:30:16 +0100

    dropElements write test 1

--- spec/dropElements.spec.js
+++ spec/dropElements.spec.js
@@ -2,5 +2,11 @@ const { expect } = require("chai");
 const { dropElements } = require("../katas/dropElements");
 
 describe("dropElements()", () => {
-  it("", () => {});
+  it("Should return an empty array if passed a an empty array and a function", () => {
+    expect(
+      dropElements([], function(n) {
+        return n >= 3;
+      })
+    );
+  });
 });
