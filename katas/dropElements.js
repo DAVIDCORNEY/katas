0aa320469882e069dd2737ed560b577dff4b807a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 14:37:47 +0100

    dropElements refactor function

--- katas/dropElements.js
+++ katas/dropElements.js
@@ -1,13 +1,8 @@
 function dropElements(arr, func) {
-  let index = 0;
-  if (arr.length === 0) return arr;
   for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i])) {
-      index = i;
-      break;
-    }
+    if (func(arr[i])) return arr.slice(i);
   }
-  return arr.slice(index);
+  return [];
 }
 
 module.exports = { dropElements };
