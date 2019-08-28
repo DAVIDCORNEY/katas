3011841ba463c06f685e3010b157b3862b2c4922
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 07:33:23 +0100

    smallestCommons all tests passing remove .only

--- spec/smallestCommons.spec.js
+++ spec/smallestCommons.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { smallestCommons } = require("../katas/smallestCommons");
 
-describe.only("smallestCommons()", () => {
+describe("smallestCommons()", () => {
   it("should return a number when passed an array of two numbers", () => {
     expect(smallestCommons([1, 2])).to.be.a("number");
   });
