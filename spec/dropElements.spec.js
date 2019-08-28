ae48398940fa3bfe3fcb25a4b03dd19f4a3464d8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 07:46:27 +0100

    dropElements write test 2

--- spec/dropElements.spec.js
+++ spec/dropElements.spec.js
@@ -5,8 +5,15 @@ describe.only("dropElements()", () => {
   it("Should return an empty array if passed a an empty array and a function", () => {
     expect(
       dropElements([], function(n) {
-        return n >= 3;
+        return n >= 1;
       })
+    ).to.eql([]);
+  });
+  it("Should return an array filtered by whether it is >= to a number stated in the function starting from the first element until the function returns true", () => {
+    expect(
+      dropElements([1], function(n) {
+        return n >= 1;
+      }).to.eql([1])
     );
   });
 });
