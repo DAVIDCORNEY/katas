3b51c16efdc7b5dd77903022d31d0cca466a0033
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-29 07:49:50 +0100

    steamRoller write 1st test

--- spec/steamRoller.spec.js
+++ spec/steamRoller.spec.js
@@ -2,5 +2,7 @@ const { expect } = require("chai");
 const { steamRoller } = require("../katas/steamRoller");
 
 describe("steamRoller()", () => {
-  it("", () => {});
+  it("should return an array if passed an array ", () => {
+    expect(steamRoller([])).to.eql([]);
+  });
 });
