adb0107382fb460ad95fb9d1773bd651dc3449c0
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-29 07:54:13 +0100

    steamRoller write test 2

--- spec/steamRoller.spec.js
+++ spec/steamRoller.spec.js
@@ -5,4 +5,7 @@ describe.only("steamRoller()", () => {
   it("should return an array if passed an array ", () => {
     expect(steamRoller([])).to.eql([]);
   });
+  it("should return an array with one element if passed an array with one element", () => {
+    expect(steamRoller([1])).to.eql([1]);
+  });
 });
