917ccca30c34e74c981807884ef026b525c06a13
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-29 07:51:30 +0100

    steamRoller add .only

--- spec/steamRoller.spec.js
+++ spec/steamRoller.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { steamRoller } = require("../katas/steamRoller");
 
-describe("steamRoller()", () => {
+describe.only("steamRoller()", () => {
   it("should return an array if passed an array ", () => {
     expect(steamRoller([])).to.eql([]);
   });
