6502a16e8f5dc6ffe1bfd5e4e8ec691c2dcbc7b6
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 08:56:02 +0100

    binaryAgent write test 3

--- spec/binaryAgent.spec.js
+++ spec/binaryAgent.spec.js
@@ -5,9 +5,12 @@ describe("binaryAgent()", () => {
   it("should return an empty string when passed an empty string", () => {
     expect(binaryAgent("")).to.equal("");
   });
-  it.only("should return an English letter or character when passed a single binary string", () => {
+  it("should return an English letter or character when passed a single binary string", () => {
     expect(binaryAgent("01000001")).to.equal("A");
     expect(binaryAgent("01100010")).to.equal("b");
     expect(binaryAgent("01001001")).to.equal("I");
+  });
+  it.only("should return an English word when passed a binary string", () => {
+    expect(binaryAgent("01001001 01110100")).to.equal("It");
   });
 });
