c85b403b23601ddffa0253f184bc204a66e05722
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 08:49:01 +0100

    binaryAgent passing test 2

--- katas/binaryAgent.js
+++ katas/binaryAgent.js
@@ -1,5 +1,10 @@
 function binaryAgent(str) {
-  return str;
+  let num = 0;
+  if (str.length === 0) return str;
+  for (let i = 0; i < str.length; i++) {
+    num = num * 2 + +str[i];
+  }
+  return String.fromCharCode(num);
 }
 
 module.exports = { binaryAgent };
