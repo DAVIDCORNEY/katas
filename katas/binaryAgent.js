f86f800c29f7722509244cdd5b78edb392e3f3a0
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 09:36:37 +0100

    binaryAgent refactored function to use parseInt

--- katas/binaryAgent.js
+++ katas/binaryAgent.js
@@ -3,13 +3,26 @@ function binaryAgent(str) {
   return str
     .split(" ")
     .map(function(char) {
-      let num = 0;
-      for (let i = 0; i < char.length; i++) {
-        num = num * 2 + +char[i];
-      }
-      return String.fromCharCode(num);
+      return String.fromCharCode(parseInt(char, 2));
     })
     .join("");
 }
 
 module.exports = { binaryAgent };
+
+//Refactored the function below:-
+// function binaryAgent(str) {
+//   if (str.length === 0) return str;
+//   return str
+//     .split(" ")
+//     .map(function(char) {
+//       let num = 0;
+//       for (let i = 0; i < char.length; i++) {
+//         num = num * 2 + +char[i];
+//       }
+//       return String.fromCharCode(num);
+//     })
+//     .join("");
+// }
+
+// module.exports = { binaryAgent };
