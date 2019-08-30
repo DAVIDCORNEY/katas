45e18dd37b54f6844813a246c554ec298f815d49
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 09:12:08 +0100

    binaryAgent write test 4

--- spec/binaryAgent.spec.js
+++ spec/binaryAgent.spec.js
@@ -19,4 +19,11 @@ describe.only("binaryAgent()", () => {
       )
     ).to.equal("FreeCodeCamp");
   });
+  it("should return an English sentance when passed a binary string", () => {
+    expect(
+      binaryAgent(
+        "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+      )
+    ).to.equal("Aren't bonfires fun!?");
+  });
 });
