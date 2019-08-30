1eff55829c01651cab1abb0c94ee38466738f098
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 07:53:13 +0100

    binaryAgent write test 1

--- spec/binaryAgent.spec.js
+++ spec/binaryAgent.spec.js
@@ -2,5 +2,7 @@ const { expect } = require("chai");
 const { binaryAgent } = require("binaryAgent");
 
 describe("binaryAgent()", () => {
-  it("", () => {});
+  it("should return a string when passed a string", () => {
+    expect(binaryAgent("")).to.equal("");
+  });
 });
