4f6334ed29f91fdfd60a06bd07858fe2a9c9eba4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 08:48:49 +0100

    binaryAgent move .only

--- spec/binaryAgent.spec.js
+++ spec/binaryAgent.spec.js
@@ -1,11 +1,11 @@
 const { expect } = require("chai");
 const { binaryAgent } = require("../katas/binaryAgent");
 
-describe.only("binaryAgent()", () => {
+describe("binaryAgent()", () => {
   it("should return an empty string when passed an empty string", () => {
     expect(binaryAgent("")).to.equal("");
   });
-  it("should return an English letter or character when passed a single binary string", () => {
+  it.only("should return an English letter or character when passed a single binary string", () => {
     expect(binaryAgent("01000001")).to.equal("A");
   });
 });
