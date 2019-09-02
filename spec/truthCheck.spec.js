4d8416af9d25481af3f72e8b07a717f2e53efd67
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 08:11:07 +0100

    truthCheck.spec write test 2

--- spec/truthCheck.spec.js
+++ spec/truthCheck.spec.js
@@ -2,8 +2,12 @@ const { expect } = require("chai");
 const { truthCheck } = require("../katas/truthCheck");
 
 describe.only("truthCheck()", () => {
-  it("returns a boolean when passed an array of a single key value pair and a predicate", () => {
+  it("returns a boolean when passed an array of an object with a single key value pair and a predicate", () => {
     const input = [{ sex: "Tinky-Winky" }];
+    expect(truthCheck(input, "sex")).to.eql(true);
+  });
+  it("returns a boolean when passed an array of an object with multiple key value pairs and a predicate", () => {
+    const input = [{ user: "Tinky-Winky", sex: "male" }];
     expect(truthCheck(input, "sex")).to.eql(true);
   });
 });
