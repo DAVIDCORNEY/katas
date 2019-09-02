a823fee64618e0249a7f6b620262b793c8d7d92d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 08:06:41 +0100

    truthCheck.spec write test 1

--- spec/truthCheck.spec.js
+++ spec/truthCheck.spec.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const { truthCheck } = require("../katas/truthCheck");
 
-describe("truthCheck()", () => {
-  it("", () => {});
+describe.only("truthCheck()", () => {
+  it("returns a boolean when passed an array of a single key value pair and a predicate", () => {
+    const input = [{ sex: "Tinky-Winky" }];
+    expect(truthCheck(input, "sex")).to.eql(true);
+  });
 });
