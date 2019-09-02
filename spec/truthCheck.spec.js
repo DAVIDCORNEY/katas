cfd15d292126952062902234b5c8bc4ff316e44b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 08:47:23 +0100

    truthCheck remove .only

--- spec/truthCheck.spec.js
+++ spec/truthCheck.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { truthCheck } = require("../katas/truthCheck");
 
-describe.only("truthCheck()", () => {
+describe("truthCheck()", () => {
   it("returns a boolean when passed an array of an object with a single key value pair and a predicate", () => {
     const input = [{ sex: "Tinky-Winky" }];
     expect(truthCheck(input, "sex")).to.eql(true);
