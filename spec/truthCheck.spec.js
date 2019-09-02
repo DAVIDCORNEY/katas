c88cc2e8fb2486e9d02c068b56578cb15db37104
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 08:15:31 +0100

    passing test 1 for false

--- spec/truthCheck.spec.js
+++ spec/truthCheck.spec.js
@@ -5,6 +5,7 @@ describe.only("truthCheck()", () => {
   it("returns a boolean when passed an array of an object with a single key value pair and a predicate", () => {
     const input = [{ sex: "Tinky-Winky" }];
     expect(truthCheck(input, "sex")).to.eql(true);
+    expect(truthCheck(input, "age")).to.eql(false);
   });
   it("returns a boolean when passed an array of an object with multiple key value pairs and a predicate", () => {
     const input = [{ user: "Tinky-Winky", sex: "male" }];
