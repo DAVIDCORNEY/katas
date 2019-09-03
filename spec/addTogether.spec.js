66041dfc2afc81d4b0384cbe756012d96f9c863a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-03 12:37:52 +0100

    addTogether revise test 1 to check for undefined values

--- spec/addTogether.spec.js
+++ spec/addTogether.spec.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { addTogether } = require("../katas/addTogether");
 
 describe.only("addTogether()", () => {
-  it("should return the sum of two arguments if passed two numbers as arguments", () => {
-    expect(addTogether(1, 2)).to.equal(3);
+  it("should return undefined if the passed arguments are not numbers", () => {
+    expect(addTogether(1, "2")).to.be.undefined;
   });
 });
