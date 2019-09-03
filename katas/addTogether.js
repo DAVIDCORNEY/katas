a408addf1b1e9c5259bdf96ed2970189e1852b05
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-03 13:58:04 +0100

    addTogether passing test 3

--- katas/addTogether.js
+++ katas/addTogether.js
@@ -2,8 +2,10 @@ function addTogether(...num) {
   const nums = num.every(item => {
     return typeof item === "number";
   });
-  if (nums !== false && num.length > 1) {
+  if (nums !== false && num.length === 2) {
     return num[0] + num[1];
+  } else if (nums !== false && num.length === 1) {
+    return function(num1) {};
   }
   return undefined;
 }
