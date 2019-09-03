ee86c69e92e4353935ae67c6a89dc6d6a0a6fb9b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-03 11:34:00 +0100

    addTogether.spec add .only

--- spec/addTogether.spec.js
+++ spec/addTogether.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { addTogether } = require("../katas/addTogether");
 
-describe("addTogether()", () => {
+describe.only("addTogether()", () => {
   it("should return the sum of two arguments if passed two argumets", () => {
     expect(addTogether(1, 2)).to.equal(3);
   });
