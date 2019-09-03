c0c42711a97434cfbeb2f4618f625f370266d4fd
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-03 12:48:59 +0100

    addTogether.spec re-phrase test 1

--- spec/addTogether.spec.js
+++ spec/addTogether.spec.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { addTogether } = require("../katas/addTogether");
 
 describe.only("addTogether()", () => {
-  it("should return undefined if the passed arguments are not numbers", () => {
+  it("should return undefined if any passed arguments are not numbers", () => {
     expect(addTogether(1, "2")).to.be.undefined;
   });
 });
