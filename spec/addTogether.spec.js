c7631a785fc22e09c7c49a5ea0ddf61d2ab74d7a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-03 14:30:14 +0100

    addTogether passing all tests remove .only

--- spec/addTogether.spec.js
+++ spec/addTogether.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { addTogether } = require("../katas/addTogether");
 
-describe.only("addTogether()", () => {
+describe("addTogether()", () => {
   it("should return undefined if any passed arguments are not numbers", () => {
     expect(addTogether("1")).to.be.undefined;
     expect(addTogether(1, "2")).to.be.undefined;
