da0874d876fe2c44b98d4d04fd1be0739f842f4c
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-04 21:12:52 +0100

    orbitalPeriod passing test4

--- spec/orbitalPeriod.spec.js
+++ spec/orbitalPeriod.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { orbitalPeriod } = require("../katas/orbitalPeriod");
 
-describe("orbitalPeriod()", () => {
+describe.only("orbitalPeriod()", () => {
   it("should return a new empty array when passed an array", () => {
     const input = [];
     const expected = [];
