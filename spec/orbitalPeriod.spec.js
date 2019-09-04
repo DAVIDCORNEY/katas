9c38d2567cb5c4a1358a98276cfc8b479a980ff4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-04 17:35:31 +0100

    orbitalPeriod rewrite test2 to test change of object key

--- spec/orbitalPeriod.spec.js
+++ spec/orbitalPeriod.spec.js
@@ -8,9 +8,10 @@ describe.only("orbitalPeriod()", () => {
     expect(orbitalPeriod(input)).to.not.equal(expected);
     expect(orbitalPeriod(input)).to.eql(expected);
   });
-  it("should return a new array containing the properties 'name' and 'avgAlt' when passed an array with an object with name and avgAlt", () => {
+  it("should return a new array containing the properties 'name' and 'orbitalPeriod' when passed an array with an object with name and avgAlt", () => {
     const input = [{ name: "a", avgAlt: 1 }];
-    expect(orbitalPeriod(input)).to.have.property("name");
-    expect(orbitalPeriod(input)).to.have.property("avgAlt");
+    const result = orbitalPeriod(input);
+    expect(result[0]).to.have.property("name");
+    expect(result[0]).to.have.property("orbitalPeriod");
   });
 });
