8ddf24fc5e9d859ead1d7a6fb657465e672da056
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-04 17:36:41 +0100

    orbitalPeriod write logic to change key from avgAlt to orbitalPeriod

--- katas/orbitalPeriod.js
+++ katas/orbitalPeriod.js
@@ -1,7 +1,17 @@
 function orbitalPeriod(arr) {
   if (arr.length === 0) return [];
-  const newArr = [...arr];
-  return newArr[0];
+  const newArr = arr.map(obj => {
+    const newObj = {};
+    for (let key in obj) {
+      if (key === "avgAlt") {
+        newObj["orbitalPeriod"] = obj[key];
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  });
+  return newArr;
 }
 
 module.exports = { orbitalPeriod };
