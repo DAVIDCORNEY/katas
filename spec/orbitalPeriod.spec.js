2d49dfe8df1bc464764b6adf0cf36243ef6a387e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-04 15:56:31 +0100

    orbitalPeriod write test2

--- spec/orbitalPeriod.spec.js
+++ spec/orbitalPeriod.spec.js
@@ -8,4 +8,9 @@ describe.only("orbitalPeriod()", () => {
     expect(orbitalPeriod(input)).to.not.equal(expected);
     expect(orbitalPeriod(input)).to.eql(expected);
   });
+  it("should return a new array containing the properties 'name' and 'avgAlt' when passed an array with an object with name and avgAlt", () => {
+    const input = [{ name: "a", avgAlt: 1 }];
+    expect(orbitalPeriod(input)).to.have.own.property("name");
+    expect(orbitalPeriod(input)).to.have.own.property("avgAlt");
+  });
 });
