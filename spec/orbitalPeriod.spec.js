8bf88c79ebec1ecb3f82fb63004b8e0aa8da949d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-04 15:39:24 +0100

    orbitalPeriod write test1

--- spec/orbitalPeriod.spec.js
+++ spec/orbitalPeriod.spec.js
@@ -1,6 +1,11 @@
 const { expect } = require("chai");
 const { orbitalPeriod } = require("../katas/orbitalPeriod");
 
-describe("orbitalPeriod()", () => {
-  it("", () => {});
+describe.only("orbitalPeriod()", () => {
+  it("should return a new empty array when passed an array", () => {
+    const input = [];
+    const expected = [];
+    expect(orbitalPeriod(input)).to.not.equal(expected);
+    expect(orbitalPeriod(input)).to.eql(expected);
+  });
 });
