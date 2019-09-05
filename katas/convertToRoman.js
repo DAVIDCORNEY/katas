8d56227075db8303e5fae63e2e52c2f9e55fe217
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-05 15:52:54 +0100

    convertToRoman write logic to pass test 2

--- katas/convertToRoman.js
+++ katas/convertToRoman.js
@@ -1,8 +1,15 @@
 function convertToRoman(num) {
-  const roman = {
-    1: "I"
-  };
-  return roman[num];
+  const roman = ["X", "IX", "V", "IV", "I"];
+  const nums = [10, 9, 5, 4, 1];
+  let newStr = "";
+  //if (nums.indexOf(num) > -1) return roman[nums.indexOf(num)];
+  for (let i = 0; i < nums.length; i++) {
+    while (nums[i] <= num) {
+      newStr += roman[i];
+      num -= nums[i];
+    }
+  }
+  return newStr;
 }
 
 module.exports = { convertToRoman };
