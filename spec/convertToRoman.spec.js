968b944eda5740bb47cc11f11f2b4ba63e6632eb
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-05 16:50:31 +0100

    convertToRoman spec remove .only

--- spec/convertToRoman.spec.js
+++ spec/convertToRoman.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { convertToRoman } = require("../katas/convertToRoman");
 
-describe.only("convertToRoman()", () => {
+describe("convertToRoman()", () => {
   it("should return I when passed 1 ", () => {
     expect(convertToRoman(1)).to.equal("I");
   });
