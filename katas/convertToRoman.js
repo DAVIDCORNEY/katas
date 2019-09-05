7a37be351b54a48babc73f7b542aa76088ba26d8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-05 16:06:24 +0100

    convertToRoman.js add extra numerals and  numbers in respective arrays

--- katas/convertToRoman.js
+++ katas/convertToRoman.js
@@ -1,6 +1,20 @@
 function convertToRoman(num) {
-  const roman = ["L", "X", "IX", "V", "IV", "I"];
-  const nums = [10, 9, 5, 4, 1];
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+  const nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   let newStr = "";
   //if (nums.indexOf(num) > -1) return roman[nums.indexOf(num)];
   for (let i = 0; i < nums.length; i++) {
