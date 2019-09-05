dd3b4d7e632d6e18a1505b09f584cc038e650b8c
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-05 12:19:14 +0100

    convertToRoman test 1 to equal a roman numeral string

--- spec/convertToRoman.spec.js
+++ spec/convertToRoman.spec.js
@@ -3,6 +3,6 @@ const { convertToRoman } = require("../katas/convertToRoman");
 
 describe.only("convertToRoman()", () => {
   it("should return I when passed 1 ", () => {
-    expect(convertToRoman(1)).to.equal(I);
+    expect(convertToRoman(1)).to.equal("I");
   });
 });
