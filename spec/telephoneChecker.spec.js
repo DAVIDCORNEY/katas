d83596473f2d38de0ab7b03edcc1d20bf360ce83
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-05 19:57:34 +0100

    telephoneChecker.spec write test2

--- spec/telephoneChecker.spec.js
+++ spec/telephoneChecker.spec.js
@@ -5,4 +5,7 @@ describe.only("telephoneChecker()", () => {
   it("should return a boolean when passed a string", () => {
     expect(telephoneChecker("555-555-5555")).to.be.a("boolean");
   });
+  it("should return true when passed a US phone number string of the format '555-555-5555'", () => {
+    expect(telephoneChecker("555-555-5555")).to.equal(true);
+  });
 });
