f48cc8970428c73141eae339df563e1118fa8572
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-06 13:57:32 +0100

    telephoneChecker write test 9

--- spec/telephoneChecker.spec.js
+++ spec/telephoneChecker.spec.js
@@ -26,4 +26,7 @@ describe.only("telephoneChecker()", () => {
   it("should return true when passed a US phone number string of the format '5555555555'", () => {
     expect(telephoneChecker("5555555555")).to.be.true;
   });
+  it("should return true when passed a US phone number string of the format '1 555 555 5555' ", () => {
+    expect(telephoneChecker("1 456 789 4444")).to.be.true;
+  });
 });
