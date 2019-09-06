c05dbb73cf0008896c12ab34d1d2a27a0069b58d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-06 15:59:56 +0100

    telephoneChecker remove .only passing all tests

--- spec/telephoneChecker.spec.js
+++ spec/telephoneChecker.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { telephoneChecker } = require("../katas/telephoneChecker");
 
-describe.only("telephoneChecker()", () => {
+describe("telephoneChecker()", () => {
   it("should return a boolean when passed a string", () => {
     expect(telephoneChecker("555-555-5555")).to.be.a("boolean");
   });
