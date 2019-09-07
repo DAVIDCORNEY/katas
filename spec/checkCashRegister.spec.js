921db7f73ce68e891c23c85fd18c357df6380282
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-07 11:36:40 +0100

    checkCashRegister write test 3

--- spec/checkCashRegister.spec.js
+++ spec/checkCashRegister.spec.js
@@ -17,7 +17,7 @@ describe.only("checkCashRegister()", () => {
       ])
     ).to.be.an("object");
   });
-  it("should return an object with key value pairs of status:'closed' and change: set to the cid array, if the change due is equal to the value of cash in the till ", () => {
+  it("should return an object with key value pairs of status:'CLOSED' and change: set to the cid array, if the change due is equal to the value of cash in the till ", () => {
     expect(
       checkCashRegister(19.5, 20, [
         ["PENNY", 0.5],
@@ -44,5 +44,20 @@ describe.only("checkCashRegister()", () => {
         ["ONE HUNDRED", 0]
       ]
     });
+  });
+  it("should return an object with key value pairs of status:'INSUFFICIENT_FUNDS' and change: set to an empty array, if the change due is greater than the value of cash in the till ", () => {
+    expect(
+      checkCashRegister(19.5, 20, [
+        ["PENNY", 0.01],
+        ["NICKEL", 0],
+        ["DIME", 0],
+        ["QUARTER", 0],
+        ["ONE", 1],
+        ["FIVE", 0],
+        ["TEN", 0],
+        ["TWENTY", 0],
+        ["ONE HUNDRED", 0]
+      ])
+    ).to.eql({ status: "INSUFFICIENT_FUNDS", change: [] });
   });
 });
