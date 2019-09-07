b7cd4a5853c8f7b9720207864b9a9ed9b699dc48
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-07 10:16:12 +0100

    checkCashRegister write test 1

--- spec/checkCashRegister.spec.js
+++ spec/checkCashRegister.spec.js
@@ -2,5 +2,19 @@ const { expect } = require("chai");
 const { checkCashRegister } = require("checkCashRegister");
 
 describe("checkCashRegister()", () => {
-  it("", () => {});
+  it("should return an object when passed an array", () => {
+    expect(
+      checkCashRegister(19.5, 20, [
+        ["PENNY", 1.01],
+        ["NICKEL", 2.05],
+        ["DIME", 3.1],
+        ["QUARTER", 4.25],
+        ["ONE", 90],
+        ["FIVE", 55],
+        ["TEN", 20],
+        ["TWENTY", 60],
+        ["ONE HUNDRED", 100]
+      ])
+    ).to.eql({ status: "OPEN", change: [["QUARTER", 0.5]] });
+  });
 });
