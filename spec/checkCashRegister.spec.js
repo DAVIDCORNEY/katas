84644fef9584860bb86081152bd19bc99cc270f0
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-07 11:16:13 +0100

    checkCashRegister write test 2

--- spec/checkCashRegister.spec.js
+++ spec/checkCashRegister.spec.js
@@ -17,4 +17,32 @@ describe.only("checkCashRegister()", () => {
       ])
     ).to.be.an("object");
   });
+  it("should return an object with key value pairs of status:'closed' and change: set to the cid array ", () => {
+    expect(
+      checkCashRegister(19.5, 20, [
+        ["PENNY", 0.5],
+        ["NICKEL", 0],
+        ["DIME", 0],
+        ["QUARTER", 0],
+        ["ONE", 0],
+        ["FIVE", 0],
+        ["TEN", 0],
+        ["TWENTY", 0],
+        ["ONE HUNDRED", 0]
+      ])
+    ).to.eql({
+      status: "CLOSED",
+      change: [
+        ["PENNY", 0.5],
+        ["NICKEL", 0],
+        ["DIME", 0],
+        ["QUARTER", 0],
+        ["ONE", 0],
+        ["FIVE", 0],
+        ["TEN", 0],
+        ["TWENTY", 0],
+        ["ONE HUNDRED", 0]
+      ]
+    });
+  });
 });
