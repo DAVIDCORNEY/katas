b644088d5851492b22d5374ba9df5b08cace3da6
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-07 11:33:23 +0100

    checkCashRegister write logic to pass test 2

--- katas/checkCashRegister.js
+++ katas/checkCashRegister.js
@@ -1,9 +1,20 @@
 function checkCashRegister(price, cash, cid) {
-  const change = {
+  const changeDue = cash - price;
+  const tillTotal = cid.reduce((acc, curr) => acc + curr[1], 0);
+  const changeStatus = {
     status: "OPEN",
-    change: [[]]
+    change: []
   };
-  return change;
+  if (changeDue === tillTotal) {
+    changeStatus["status"] = "CLOSED";
+    changeStatus["change"] = cid;
+  }
+  return changeStatus;
 }
 
 module.exports = { checkCashRegister };
+
+//work out the difference between the price and cash given and store in a variable "changeDue"
+//work out the total value of all the cash in the till and store in a variable "tillTotal"
+//if tillTotal is less than changeDue then return {status: "INSUFFICIENT_FUNDS", change: []}
+//if tillTotal is the same as changeDue then return {status: "CLOSED", change: [all cid]}
