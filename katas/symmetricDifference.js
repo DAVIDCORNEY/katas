a897667907e47bef1a15907214ac7cda0f70810f
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-23 12:57:31 +0100

    sym write logic to pass test 4

--- katas/symmetricDifference.js
+++ katas/symmetricDifference.js
@@ -1,9 +1,12 @@
 function sym(args) {
-  if (args.length === 0) {
-    return [];
-  } else {
-    return args;
-  }
+  if (args.length === 0) return [];
+  const symmetricNums = args.reduce((total, curr) => {
+    if (!total.includes(curr)) {
+      total.push(curr);
+    }
+    return total;
+  }, []);
+  return symmetricNums;
 }
 
 module.exports = { sym };
