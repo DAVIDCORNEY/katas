d1447fad240a24eb89394eaf6675936f93a640db
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-23 12:11:11 +0100

    sym write first test

--- spec/symmetricDifference.spec.js
+++ spec/symmetricDifference.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
-const { sym } = require(sym);
+const { sym } = require("../katas/symmetricDifference");
 
-describe("sym", () => {
-  it("", () => {});
+describe.only("sym", () => {
+  it("will return an empty array when passed an empty array", () => {
+    expect(sym([])).to.eql([]);
+  });
 });
