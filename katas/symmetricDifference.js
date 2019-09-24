c60f5f736042c0c84f2642d8415671f3ed81321e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-24 14:58:53 +0100

    sym write logic to pass test 6

--- katas/symmetricDifference.js
+++ katas/symmetricDifference.js
@@ -1,24 +1,24 @@
 function sym(...args) {
-  if (args[0].length === 0) return [];
-  const symmetricNums = args.map(function(arg) {
-    return arg.reduce((total, curr) => {
-      if (!total.includes(curr)) {
-        total.push(curr);
+  const arguments = [...args];
+
+  function getSymDiff(array1, array2) {
+    const symmetricalNums = [];
+    array1.forEach(num => {
+      if (array2.indexOf(num) < 0 && symmetricalNums.indexOf(num) < 0) {
+        symmetricalNums.push(num);
       }
-      return total;
-    }, []);
-  });
-  const synNums = [].concat(...symmetricNums).sort((a, b) => a - b);
-  const newArr = [];
-  for (let i = 0; i < synNums.length; i++) {
-    if (
-      synNums.indexOf(synNums[i]) === i &&
-      synNums.indexOf(synNums[i], i + 1) === -1
-    ) {
-      newArr.push(synNums[i]);
-    }
+    });
+    array2.forEach(num => {
+      if (array1.indexOf(num) < 0 && symmetricalNums.indexOf(num) < 0) {
+        symmetricalNums.push(num);
+      }
+    });
+    return symmetricalNums;
   }
-  return newArr;
+  return arguments
+    .reduce(getSymDiff)
+    .filter((num, index, array) => array.indexOf(num) === index)
+    .sort((a, b) => a - b);
 }
 
 module.exports = { sym };
