89487a4d1430cfb87035f9a116a36c42d6b50daa
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-01 14:02:59 +0100

    updateInventory write logic to pass test 3

--- katas/updateInventory.js
+++ katas/updateInventory.js
@@ -2,7 +2,7 @@ function updateInventory(arr1, arr2) {
   if (arr2.length === 0) return arr1;
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
-      if (arr2[j].indexOf(arr1[i][1])) {
+      if (arr2[j].indexOf(arr1[i][1]) > 0) {
         arr1[i][0] = arr1[i][0] + arr2[j][0];
       }
     }
