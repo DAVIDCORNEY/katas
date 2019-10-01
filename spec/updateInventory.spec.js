07fdf15912cba01fe6daa80db1cab076b9465060
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-01 13:00:31 +0100

    updateInventory revise test 1

--- spec/updateInventory.spec.js
+++ spec/updateInventory.spec.js
@@ -3,6 +3,8 @@ const { updateInventory } = require("../katas/updateInventory");
 
 describe.only("updateInventory", () => {
   it("should return array1 unchanged when array2 is passed as an empty array", () => {
-    expect(updateInventory([[21, "Bowling Ball"]], [])).to.eql([]);
+    expect(updateInventory([[21, "Bowling Ball"]], [])).to.eql([
+      [21, "Bowling Ball"]
+    ]);
   });
 });
