fdc8df806c52864693face05c18c62b533d11535
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-01 17:10:08 +0100

    updateInventory remove .only passing all tests

--- spec/updateInventory.spec.js
+++ spec/updateInventory.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { updateInventory } = require("../katas/updateInventory");
 
-describe.only("updateInventory", () => {
+describe("updateInventory", () => {
   it("should return array1 unchanged when array2 is passed as an empty array", () => {
     expect(updateInventory([[21, "Bowling Ball"]], [])).to.eql([
       [21, "Bowling Ball"]
