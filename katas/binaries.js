3c3b0084db9d2ee4e243a0e9da7ae5f35dc519a5
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-13 16:15:45 +0100

    binaries passing test 1

--- katas/binaries.js
+++ katas/binaries.js
@@ -1,3 +1,9 @@
-function binaries() {}
+function binaries(str) {
+  let binary = str.toString(2);
+  let binaryLength = binary.length;
+  let preBinary = "0".repeat(binaryLength - 1) + "1";
+  let code = preBinary + binary;
+  return code;
+}
 
 module.exports = { binaries };
