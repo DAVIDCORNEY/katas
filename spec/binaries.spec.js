b778d12a1164e774617f2057ea0b1bc70e446638
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-13 16:30:49 +0100

    binaries write test 3

--- spec/binaries.spec.js
+++ spec/binaries.spec.js
@@ -8,4 +8,7 @@ describe.only("binaries()", () => {
   it("Should return 11 when passed string of 1", () => {
     expect(binaries("1")).to.equal("11");
   });
+  it("Should return 0110 when passed string of 2", () => {
+    expect(binaries("2")).to.equal("0110");
+  });
 });
