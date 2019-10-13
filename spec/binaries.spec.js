81c691ee2470c756722e5c6ee497cc2ee8139b27
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-13 15:56:40 +0100

    binaries spec add .only

--- spec/binaries.spec.js
+++ spec/binaries.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { binaries } = require("../katas/binaries");
 
-describe("binaries()", () => {
+describe.only("binaries()", () => {
   it("Should return 10 when passed string of 0", () => {
     expect(binaries("0")).to.equal("10");
   });
