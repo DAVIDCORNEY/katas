45332acbe765a8e8f04d78492bf8954859634da1
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-14 14:13:36 +0100

    subPattern write logic to pass test 3

--- katas/subPattern1.js
+++ katas/subPattern1.js
@@ -2,7 +2,7 @@ function subPattern1(str) {
   if (str.length < 2) return false;
   let firstHalf = str.substring(0, str.length / 2);
   let secondHalf = str.substring(str.length / 2, str.length);
-  if (firstHalf === secondHalf) return true;
+  return firstHalf === secondHalf ? true : false;
 }
 
 module.exports = { subPattern1 };
