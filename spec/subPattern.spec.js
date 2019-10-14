eedb674117fbab7f9a0c6f71b09862281b5ad366
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-14 13:47:04 +0100

    subPattern write 1st test

--- spec/subPattern.spec.js
+++ spec/subPattern.spec.js
@@ -2,5 +2,7 @@ const { expect } = require("chai");
 const { subPattern } = require("../katas/subPattern1");
 
 describe("subPattern()", () => {
-  it("should return false when passed a string with one char", () => {});
+  it("should return false when passed a string with one char", () => {
+    expect(subPattern("a")).to.be.false;
+  });
 });
