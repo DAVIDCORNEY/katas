cfbc17109d9a873841c1a5fbb25b7e2bf345cf9d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-14 13:45:52 +0100

    Setup subPattern spec file and describe block

---
+++ spec/subPattern.spec.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { subPattern } = require("../katas/subPattern1");
+
+describe("subPattern()", () => {
+  it("should return false when passed a string with one char", () => {});
+});
