c55401b1bcd741d643d51fbaa3bf4853e4485cc3
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-14 14:42:06 +0100

    subPattern spec write test 5

--- spec/subPattern1.spec.js
+++ spec/subPattern1.spec.js
@@ -14,4 +14,7 @@ describe.only("subPattern1()", () => {
   it("should return true when passed a string with three chars that are the same", () => {
     expect(subPattern1("aaa")).to.be.true;
   });
+  it("should return false when passed a string with three chars that are not the same", () => {
+    expect(subPattern1("abc")).to.be.false;
+  });
 });
