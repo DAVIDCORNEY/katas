4f0a398b813a2d171170c7dfb3b937443c649231
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-23 21:04:48 +0100

    wave js write logic to pass test 2

--- katas/wave.js
+++ katas/wave.js
@@ -1,5 +1,20 @@
 function wave(str) {
+  const waveArr = [];
+  let count = 0;
+  let waveStr = "";
   if (str.length === 0) return [];
+  while (count < str.length) {
+    for (let i = 0; i < str.length; i++) {
+      if (i === count) {
+        waveStr += str[i].toUpperCase();
+      } else {
+        waveStr += str[i];
+      }
+      count++;
+    }
+    waveArr.push(waveStr);
+  }
+  return waveArr;
 }
 
 module.exports = { wave };
