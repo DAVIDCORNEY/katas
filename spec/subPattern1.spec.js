c4db81325beee44e2c34927d023454d8d9dfda04
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-23 13:14:57 +0100

    subPattern1 remove .only

--- spec/subPattern1.spec.js
+++ spec/subPattern1.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { subPattern1 } = require("../katas/subPattern1");
 
-describe.only("subPattern1()", () => {
+describe("subPattern1()", () => {
   it("should return false when passed a string with one char", () => {
     expect(subPattern1("a")).to.be.false;
   });
