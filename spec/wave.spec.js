24bb648203b60fb047b915dacd4a45a8affee454
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-23 21:04:30 +0100

    wave spec write test 2

--- spec/wave.spec.js
+++ spec/wave.spec.js
@@ -5,4 +5,7 @@ describe.only("wave()", () => {
   it("should return an empty array if passed an empty string", () => {
     expect(wave("")).to.eql([]);
   });
+  it("should return a array with a single letter capitalised if passed a string of a single letter", () => {
+    expect(wave("a")).to.eql(["A"]);
+  });
 });
