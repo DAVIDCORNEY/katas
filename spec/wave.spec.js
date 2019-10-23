05cdf6f10aa32bd0beb3ca1ef74d8ab01c4460cb
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-23 13:15:32 +0100

    wave spec write test 1

--- spec/wave.spec.js
+++ spec/wave.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
 const { wave } = require("../katas/wave");
 
-describe("wave()", () => {
-  it("", () => {});
+describe.only("wave()", () => {
+  it("should return an empty array if passed an empty string", () => {
+    expect(wave("")).to.eql([]);
+  });
 });
